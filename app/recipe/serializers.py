cdb39f2e82271b94ed2b79a0f03461edf3c8161b
Cesar <cesarc@nicasource.com>
2023-10-27 16:04:31 -0600

    Added list ingredients feature.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -8,5 +8,15 @@ class TagSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id', )
+
+
+class IngredientsSerializer(serializers.ModelSerializer):
+    """
+      Serializer for ingredients objects
+    """
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id', )
